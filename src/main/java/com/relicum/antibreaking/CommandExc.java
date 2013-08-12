cfe0b51e1853adac07898b83920ecb020c574d2d
Relicum <chrislutte@relicum.com>
2013-08-12 14:28:42 +0100

    Setplace command 95% working

--- src/main/java/com/relicum/antibreaking/CommandExc.java
+++ src/main/java/com/relicum/antibreaking/CommandExc.java
@@ -6,6 +6,9 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Antibreaking
  *
@@ -38,12 +41,105 @@ public class CommandExc implements CommandExecutor {
         } else if (label.equalsIgnoreCase("setplace")) {
             if (sender instanceof Player) {
                 Player player = (Player) sender;
+                String wo;
+                String setTo;
+                System.out.println("Get Here total args is " + args.length + Arrays.toString(args));
+                if (args.length == 0) {
+                    player.sendMessage(ChatColor.DARK_RED + "You arguments length can not be 0. Must pass on or off, world is optional");
+                    return true;
+                }
+
+                setTo = args[0];
+                System.out.println(setTo);
+
+                if (!((!setTo.equals("On")) && (!setTo.equals("Off")))) {
+                    player.sendMessage(ChatColor.DARK_RED + "The first argument must be On or Off currently is " + args[0]);
+                    return true;
+                }
+                System.out.println("make it here");
+                if (args.length > 1) {
+                    String world = args[1];
+                    if (!isValidWorld(world)) {
+                        player.sendMessage(ChatColor.DARK_RED + "You have not passed a valid world, worlds are case sensitive " + world);
+                        return true;
+                    }
+                    wo = world.toLowerCase();
+                } else {
+                    wo = player.getWorld().getName().toLowerCase();
+                    setTo = args[0].toLowerCase();
+                    System.out.println("World not present so using players current");
+                }
+
+                if (setTo.equals("off")) {
+                    if (removeSetting(wo, "place")) {
+                        player.sendMessage(ChatColor.GREEN + "You can now place blocks in " + wo);
+                    } else {
+                        player.sendMessage(ChatColor.GREEN + "Nothing to set you could already place blocks in " + wo);
+                    }
+                    return true;
+                } else {
+
+                    if (addSetting(wo, "place")) {
+                        player.sendMessage(ChatColor.GREEN + "You can no longer place blocks in " + wo);
+                    } else {
+                        player.sendMessage(ChatColor.GREEN + "Nothing to set you could not place blocks in " + wo + " already");
+                    }
+                    return true;
+                }
+
+
             } else {
                 plugin.getLogger().info("Setplace command can only be run in game");
-                return false;
+                return true;
             }
 
         }
+        System.out.println("Get to the bottom");
+        return false;
+    }
+
+    /**
+     * Checks if a given string is a valid world
+     *
+     * @param wo String
+     * @return boolean
+     */
+    private boolean isValidWorld(String wo) {
+
+        if (plugin.getServer().getWorld(wo) == null) {
+            return false;
+        }
         return true;
+    }
+
+    private boolean removeSetting(String world, String type) {
+
+        if (plugin.getInstance().getworldP().contains(world + type)) {
+            List<String> li = plugin.getConfig().getStringList("Worlds." + world);
+            li.remove(type);
+            plugin.getConfig().set("Worlds." + world, li);
+            plugin.saveConfig();
+            plugin.reloadConfig();
+            plugin.loadPerms();
+            return true;
+        }
+
+        return false;
+
+    }
+
+    private boolean addSetting(String world, String type) {
+
+        if (!plugin.getInstance().getworldP().contains(world + type)) {
+            List<String> li = plugin.getConfig().getStringList("Worlds." + world);
+            li.add(type);
+            plugin.getConfig().set("Worlds." + world, li);
+            plugin.saveConfig();
+            plugin.reloadConfig();
+            plugin.loadPerms();
+            return true;
+        }
+
+        return false;
     }
 }
